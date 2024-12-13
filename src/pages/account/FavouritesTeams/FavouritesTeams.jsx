ebd1040b5ab6c4c919c64023e877469376be7f9d
JesusChaves <82162116+GachaDev@users.noreply.github.com>
2024-12-13 17:31:52 +0100

    Favourites teams

--- src/pages/account/FavouritesTeams/FavouritesTeams.jsx
+++ src/pages/account/FavouritesTeams/FavouritesTeams.jsx
@@ -1,10 +1,57 @@
+import { useState, useEffect } from "react";
+import teamsData from "../../../assets/teams.json";
+
 export default function FavouritesTeams() {
+	const [teams, setTeams] = useState([]);
+	const [selectedTeam, setSelectedTeam] = useState();
+	const [favouritesTeams, setFavouritesTeams] = useState([]);
+
+	useEffect(() => {
+		setTeams(teamsData.teams);
+		setSelectedTeam(teamsData.teams[0].id);
+	}, []);
+
 	return (
 		<section>
-			<form action="">
+			<form>
 				<h1>Equipos Favoritos</h1>
-				<input type="text" placeholder="Nuevo Equipo" required />
-				<button>Añadir</button>
+				{favouritesTeams.map((team) => (
+					<div className="favourites-team" key={team}>
+						<div>
+							<img src={teams.find((t) => t.id == team).crest} />
+							<p>{teams.find((t) => t.id == team).name}</p>
+						</div>
+						<button
+							onClick={(event) => {
+								event.preventDefault();
+								setFavouritesTeams(
+									favouritesTeams.filter((t) => t !== team)
+								);
+							}}
+						>
+							X
+						</button>
+					</div>
+				))}
+				<select
+					name="team"
+					id="team"
+					onChange={(e) => {
+						setSelectedTeam(e.target.value);
+					}}
+				>
+					{teams.map((team) => (
+						<option value={team.id}>{team.name}</option>
+					))}
+				</select>
+				<button
+					onClick={(event) => {
+						event.preventDefault();
+						setFavouritesTeams([...favouritesTeams, selectedTeam]);
+					}}
+				>
+					Añadir
+				</button>
 			</form>
 		</section>
 	);
