4c80f9879b2b82e00b86057b43baa31a064d53cc
Gacha <jlgacha75@gmail.com>
2024-12-13 12:58:39 +0100

    Login

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css';
 import Header from './components/header/Header';
 import NotFound from './pages/notFound/NotFound';
 import Contact from './pages/contacto/Contact';
+import Login from './pages/login/Login';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
@@ -13,7 +14,7 @@ createRoot(document.getElementById('root')).render(
 				<Route element={<Header />}>
 					<Route index element={<h1>Home</h1>} />
 					<Route path='contacto' element={<Contact />} />
-					<Route path='login' element={<h1>Login</h1>} />
+					<Route path='login' element={<Login />} />
 					<Route path='registro' element={<h1>Registro</h1>} />
 					<Route path='*' element={<NotFound />} />
 				</Route>
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import './Login.css';
+
+export default function Login() {
+	return (
+		<div className='login'>
+			<form action=''>
+				<h1>ALLFOOTBALL</h1>
+				<input type='text' placeholder='Usuario' required />
+				<input type='password' placeholder='Contraseña' required />
+				<button type='submit'>Iniciar Sesión</button>
+				<Link to={'/registro'}>¿Has olvidado tu contraseña?</Link>
+				<div>
+					<hr />
+				</div>
+				<Link to={'/registro'}>Registro</Link>
+			</form>
+		</div>
+	);
+}
