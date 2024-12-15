b30297a52a3988872114d678b6b0824cacfd36a6
Gacha <jlgacha75@gmail.com>
2024-12-15 12:06:38 +0100

    Search

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import Login from './pages/login/Login';
 import Register from './pages/registro/Register';
 import Account from './pages/account/Account';
 import Home from './pages/home/Home';
+import Search from './pages/search/Search';
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
@@ -20,6 +21,7 @@ createRoot(document.getElementById('root')).render(
 					<Route path='login' element={<Login />} />
 					<Route path='registro' element={<Register />} />
 					<Route path='cuenta' element={<Account />} />
+					<Route path='resultadoBusqueda' element={<Search />} />
 					<Route path='*' element={<NotFound />} />
 				</Route>
 			</Routes>
