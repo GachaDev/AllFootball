cf3f84ad275c393d5f05c249d9fe4fb7595944cb
Gacha <jlgacha75@gmail.com>
2024-12-15 11:55:52 +0100

    Home

---
+++ src/components/match/Match.jsx
@@ -0,0 +1,26 @@
+export default function Match({ match }) {
+	return (
+		<div className='match'>
+			<h2>Jornada {match.matchday}</h2>
+			<div className='teams'>
+				<div className='team teamHome'>
+					<h3>{match.homeTeam.shortName}</h3>
+					<img alt={match.homeTeam.name} src={match.homeTeam.crest} />
+				</div>
+				<span>
+					{new Date(match.utcDate).toLocaleTimeString('es-ES', {
+						hour: '2-digit',
+						minute: '2-digit'
+					})}
+				</span>
+				<div className='team teamAway'>
+					<img src={match.awayTeam.crest} alt={match.awayTeam.name} />
+					<h3>{match.awayTeam.shortName}</h3>
+				</div>
+			</div>
+			<div className='television'>
+				<span>Televisado en: DAZN</span>
+			</div>
+		</div>
+	);
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,26 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
-import Header from "./components/header/Header";
-import NotFound from "./pages/notFound/NotFound";
-import Contact from "./pages/contacto/Contact";
-import Login from "./pages/login/Login";
-import Register from "./pages/registro/Register";
-import Account from "./pages/account/Account";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Header from './components/header/Header';
+import NotFound from './pages/notFound/NotFound';
+import Contact from './pages/contacto/Contact';
+import Login from './pages/login/Login';
+import Register from './pages/registro/Register';
+import Account from './pages/account/Account';
+import Home from './pages/home/Home';
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<BrowserRouter>
 			<Routes>
 				<Route element={<Header />}>
-					<Route index element={<h1>Home</h1>} />
-					<Route path="contacto" element={<Contact />} />
-					<Route path="login" element={<Login />} />
-					<Route path="registro" element={<Register />} />
-					<Route path="cuenta" element={<Account />} />
-					<Route path="*" element={<NotFound />} />
+					<Route index element={<Home />} />
+					<Route path='contacto' element={<Contact />} />
+					<Route path='login' element={<Login />} />
+					<Route path='registro' element={<Register />} />
+					<Route path='cuenta' element={<Account />} />
+					<Route path='*' element={<NotFound />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
